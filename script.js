3cc502d3844ab9c94b7f498a3f5ab46e04bd21db
Meerim <meerione11@gmail.com>
2022-02-15 18:29:36 +0600

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+alert('Hello,world');
+alert('Hello , Javascript');
+*/
+/*
+alert('error');
+[1,2].forEach(alert)*/
+
+
+/***** Peremenniye ******/
+/*
+let name = 'Meka';
+alert(name);*/
+
+/*
+let name = 'Maks';
+name = 'Dima'
+alert(name);*/
+
+/*
+let return1 = 55;
+alert(return1);*/
+
+/*
+const name = 'Meeka';
+name = 'Maks';
+alert(name);*/
+
